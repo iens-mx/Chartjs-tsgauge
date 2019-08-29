ab86d90e19e76f93f1e36050edaf3e6f8801e5f9
iens-mx <53876683+iens-mx@users.noreply.github.com>
2019-08-29 13:22:51 -0600

    Added Comma in Label

--- Gauge.js
+++ Gauge.js
@@ -4,6 +4,17 @@
 	}
 	function GaugeChartHelper() {
 	}
+	function comma(nStr){
+          nStr += '';
+          x = nStr.split('.');
+          x1 = x[0];
+          x2 = x.length > 1 ? '.' + x[1] : '';
+          var rgx = /(\d+)(\d{3})/;
+          while (rgx.test(x1)) {
+            x1 = x1.replace(rgx, '$1' + ',' + '$2');
+           }
+          return x1 + x2;
+        }
 	GaugeChartHelper.prototype.setup = function(chart, config) {
 		this.chart = chart;
 		this.ctx = chart.ctx;
@@ -112,7 +123,7 @@
 		}
 	};
 	GaugeChartHelper.prototype.renderValueLabel = function() {
-		var label = this.data.value.toString();
+		var label = comma(this.data.value.toString());
 		var ctx = this.ctx;
 		ctx.font = "30px " + this.fontStyle;
 		var stringWidth = ctx.measureText(label).width;
